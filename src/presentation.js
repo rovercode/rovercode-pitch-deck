fa83df08fa495fcc7a926ac57d80d69bf86857c1
Brady L. Hurlburt <brady.hurlburt@genesys.com>
2018-06-21 07:36:06 -0400

    Finish first pass at content

--- src/presentation.js
+++ src/presentation.js
@@ -7,6 +7,7 @@ import {
   Cite,
   Deck,
   Heading,
+  Image,
   ListItem,
   List,
   Quote,
@@ -21,95 +22,71 @@ import createTheme from 'spectacle/lib/themes/default';
 require('normalize.css');
 
 const theme = createTheme(
-  {
-    primary: '#ff4081',
-    secondary: '#FFFFFF',
-    tertiary: '#111111',
-  },
-  {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
-  }
+    {
+      primary: '#ff4081',
+      secondary: '#FFFFFF',
+      tertiary: '#111111',
+    },
+    {
+      primary: 'Montserrat',
+      secondary: 'Helvetica',
+    }
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-        progress={'none'}
-      >
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>I can't start a robotics club; I don't know anything about coding!</Quote>
-            <Cite>a teacher</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Wait, the kit costs HOW much?</Quote>
-            <Cite>a teacher trying to start a robotics club</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Ok, now we have two days left to code everything!</Quote>
-            <Cite>a robotics club coach</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={['zoom']} bgImage="https://i.imgur.com/xKMOROA.jpg">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-      </Deck>
+        <Deck
+            transition={['zoom', 'slide']}
+            transitionDuration={500}
+            theme={theme}
+            progress={'none'}
+        >
+          <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>I can't start a robotics club; I don't know anything about coding!</Quote>
+              <Cite>a teacher</Cite>
+            </BlockQuote>
+          </Slide>
+          <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>Wait, the kit costs HOW much?</Quote>
+              <Cite>a teacher trying to start a robotics club</Cite>
+            </BlockQuote>
+          </Slide>
+          <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>Ok, now we have two days left to code everything!</Quote>
+              <Cite>a robotics club coach</Cite>
+            </BlockQuote>
+          </Slide>
+          <Slide transition={['zoom']} bgImage="https://i.imgur.com/xKMOROA.jpg">
+            <Image width="700" src={"https://i.imgur.com/9a5CxZk.png"} style="padding-right:6%" margin="0px auto 0px"/>
+          </Slide>
+          <Slide transition={['fade']} bgColor="secondary" textColor="tertiary">
+            <Image width="700" src={"https://rovercode.com/static/images/rover.png"} margin="0px auto 0px"/>
+            <Text margin="10px 0 0" textColor="tertiary" size={1} fit caps>
+              motors | sensors | webcam | LEDs | speaker
+            </Text>
+          </Slide>
+          <Slide transition={['fade']} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+              Be a part of this
+            </Heading>
+            <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+              follow @rovercode on Twitter
+            </Text>
+            <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+              ask your teacher to sign up on rovercode.com
+            </Text>
+          </Slide>
+          <Slide transition={['fade']} bgColor="secondary">
+            <Image width="700" src={"https://i.imgur.com/9a5CxZk.png"} style="padding-right:6%" margin="0px auto 0px"/>
+            <Text margin="10px 0 0" textColor="primary" size={1} fit bold>
+              hit the ground coding
+            </Text>
+          </Slide>
+        </Deck>
     );
   }
 }
